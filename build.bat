d4bd27f7c80588e00438559d55169806a82fa574
Alexander Koskovich <akoskovich@cissecure.com>
2023-06-06 16:55:54 -0400

    device-flasher: v1.3.1

--- build.bat
+++ build.bat
@@ -3,4 +3,4 @@ Rem This Windows Batch file will build the Windows version of the Flasher tool.
 
 FOR /F "tokens=*" %%g IN ('git rev-list -1 HEAD') do (SET GIT_COMMIT=%%g)
 
-go build -ldflags "-X main.version=1.3.0 -X 'main.gitCommit=%GIT_COMMIT%'" .
+go build -ldflags "-X main.version=1.3.1 -X 'main.gitCommit=%GIT_COMMIT%'" .
